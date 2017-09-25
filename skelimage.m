df00d591c53938304b025b15bd171346126c43a7
Erhan Bas <erhanbas@gmail.com>
2017-09-25 17:01:05 -0400

    TODO: extending with skelimage function to extend to enable octree support

---
+++ skelimage.m
@@ -0,0 +1,197 @@
+
+% Io = squeeze(h5read(myh5,myh5prob,starts,datasiz));
+% function emptyvol = skelimage(Io,BB,brainSize)
+
+calculated_parameters = '/nrs/mouselight/SAMPLES/2016-07-18b/calculated_parameters.jl'
+params = readparams(calculated_parameters)
+%% load BB
+%%%%%%%%%%%%%%%%
+% read calculated_parameters.jl file
+% 0) distribute over lowest level
+% 1) read octtree
+% 2) read padding
+% 3) run 
+grabData(inputfolder,params)
+
+
+
+%%
+[edges,subs] = skeletonimage(Io);
+inds = edges;%zeros(size(edges));
+
+%%
+subs = subs + ones(size(subs,1),1)*BB(1:2:end)-1; % convert to original subs
+inds(:,ii) = sub2ind(brainSize,subs(:,1),subs(:,2),subs(:,3)); % convert to original inds
+
+%%
+if ~isdeployed& opt.viz
+    clear Subs1 Subs2
+    [Subs1(:,1),Subs1(:,2),Subs1(:,3)]=ind2sub(brainSize,inds(:,1));
+    [Subs2(:,1),Subs2(:,2),Subs2(:,3)]=ind2sub(brainSize,inds(:,2));
+    ii = 1:1:size(Subs1,1);
+    X = [Subs1(ii,1) Subs2(ii,1) NaN(length(ii),1)]';
+    Y = [Subs1(ii,2) Subs2(ii,2) NaN(length(ii),1)]';
+    Z = [Subs1(ii,3) Subs2(ii,3) NaN(length(ii),1)]';
+    figure,
+    plot3(X(:),Y(:),Z(:),'r--')
+    axis equal tight
+end
+% %% connectivity graph
+% connG = sparse(edges_(:,1),edges_(:,2),1,max(edges_(:)),max(edges_(:)));
+
+%%
+if isdeployed | 1
+    %%
+    fileID = fopen(outfile,'w');
+    if size(inds,2)==2
+        fprintf(fileID,'%d %d\n',inds');
+    else
+        fprintf(fileID,'%d %d %.2f\n',inds');
+    end
+    fclose(fileID);
+end
+
+function params = readparams(calculated_parameters)
+fid=fopen(calculated_parameters);
+clear params
+while 1
+    tline = fgetl(fid);
+    if ~ischar(tline), break, end
+    strs = strsplit(tline,' ');
+    if strcmp(strs{2},'nlevels')
+        params.nlevel = eval(strs{4});
+    elseif strcmp(strs{2},'shape_leaf_px')
+        params.shape_leaf_px = eval(strs{4});
+    elseif strcmp(strs{2},'voxelsize_used_um')
+        params.voxelsize_used_um = eval(strs{4});
+    elseif strcmp(strs{2},'origin_nm')
+        params.origin_nm = eval(strs{4});
+    else
+        
+    end
+    disp(tline)
+end
+fclose(fid);
+end
+
+function [edges,subs] = skeletonimage(Io)
+% given input image
+% Io = squeeze(h5read(myh5,myh5prob,starts+[500 300 100],datasiz));
+% figure, imshow(squeeze(max(Io,[],3))',[]),
+if ~any(Io(:))
+    if isdeployed |1
+        %% touch file
+        inds = [];
+        %         fileID = fopen(outfile,'w');
+        %         fclose(fileID);
+    end
+    return
+end
+
+% smooth image
+Io = smooth3(Io,'gaussian',[3 3 1]);
+Io = Io>probThr;
+%%
+if ~any(Io(:))
+    if isdeployed |1
+        %% touch file
+        inds = [];
+        %         fileID = fopen(outfile,'w');
+        %         fclose(fileID);
+    end
+    return
+end
+%% cleanup image
+s  = regionprops(Io, 'centroid','PixelIdxList','Area');
+if 0
+    % fast
+    Iout = zeros(size(Io),'single');
+    for ii=1:length(s)
+        if s(ii).Area>10
+            Iout(s(ii).PixelIdxList)=Io(s(ii).PixelIdxList);
+        end
+    end
+    Io = Iout; clear Iout;
+else
+    % memory efficient
+    for ii=1:length(s)
+        if s(ii).Area<opt.sizethreshold
+            Io(s(ii).PixelIdxList)=0;
+        end
+    end
+    
+end
+%%
+if ~any(Io(:))
+    if isdeployed |1
+        %% touch file
+        inds = [];
+        %         fileID = fopen(outfile,'w');
+        %         fclose(fileID);
+    end
+    return
+end
+%%
+% binarize it before skeletionization
+Io = Io>0;
+% run skeletonization
+% if size(Io) is big limit memory by using less number of nodes
+skel = block3d({Io},[200 200 200],fullh,1,@Skeleton3D,[]);
+skel = padarray(skel,ones(1,3),0,'both');
+%%
+% estimate radius
+Io = padarray(Io,ones(1,3),0,'both');
+bIo=bwdist(~Io);
+radskel = double(bIo.*single(skel));
+
+%%
+% get the edge pairs
+dims = size(skel);
+skelinds = find(skel);
+if isempty(skelinds)
+    % touch file
+    if isdeployed |1
+        inds = [];
+        %         fileID = fopen(outfile,'w');
+        %         fclose(fileID);
+    end
+    return
+end
+%%
+nout = length(dims);
+%Compute linear indices
+k = [1 cumprod(dims(1:end-1))];
+x = [-1:1];
+per = zeros(nout^nout,nout);
+siz = nout*ones(1,nout);
+for i=1:nout
+    s = ones(1,nout);
+    s(i) = numel(x);
+    x = reshape(x,s);
+    s = siz;
+    s(i) = 1;
+    dum = repmat(x,s);
+    per(:,i) = dum(:);
+end
+idxneig = per*k';
+idxneig((numel(idxneig)+1)/2)=[];
+
+%% get edge pairs
+E = [];
+it = 1;
+for idx = skelinds(:)'
+    inds = idx + idxneig;
+    hits = inds(skel(inds));
+    rad = radskel(idx);
+    % crop back to original size
+    E{it} = [[idx*ones(length(hits),1) hits(:) rad*ones(length(hits),1)]]';
+    it = it+1;
+end
+edges = [E{:}]'; clear E
+
+%% map onto original graph
+for ii=1:2
+    [xx,yy,zz] = ind2sub(dims,edges(:,ii)); % subs on appended crop
+    subs = [xx(:),yy(:),zz(:)]-1; % to compansate crop;
+end
+end

7afb0741c028258d9fcb90694c4e02d11ae2fbdc
Adam L. Taylor <adamlyletaylor@gmail.com>
2018-12-12 16:38:38 -0500

    Version that worked to skeletonize 2018-08-01 sample

---
+++ build_cluster_skelh5.m
@@ -0,0 +1,2 @@
+mcc -m -R -nojvm -v cluster_skelh5.m -d ./compiled  -a ./common
+cp run_cluster_skelh5_at_janelia.sh compiled
--- cluster_skelh5.m
+++ cluster_skelh5.m
@@ -25,18 +25,22 @@ if ~isdeployed
 end
 if nargin<1
     if 1
-        sample='20181001'
+        sample='20180801'
         myh5prob = '/prob0'
         exp = sprintf('%s_%s',sample,myh5prob(2:end));
-        configfile = fullfile(pwd,sprintf('./config_files/%s_config_skelh5.cfg',exp));
+        configfile = fullfile(pwd,sprintf('./config_files/%s_config_skelh5_repeat.cfg',exp));
         if 1
+            % likely breakpoint location
             mysh = sprintf('%s_%s.sh',datestr(now,'ddmmyyHHMM'),exp)
             deployment(configfile,mysh,myh5prob)
-            stop = 1;
+            %stop = 1;
+            inds = [] ;
+            return
         else
             runlocal(configfile,myh5prob)
         end
     else
+        % this part is for debugging only
         %%
         sample='20170925'
         myh5prob = '/prob0'
@@ -254,6 +258,7 @@ if isdeployed | 1
     fclose(fileID);
 end
 end
+
 function runlocal(configfile,myh5prob)
 %%
 % cluster_skelh5('/nrs/mouselight/cluster/classifierOutputs/2015-06-19/150619prob_octants12_prob0_lev-5.h5',...
@@ -330,7 +335,7 @@ unix(sprintf('chmod g+rwx %s',outfolder));
 %
 % rmdir(outfolder)
 s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-compiledfunc = '/groups/mousebrainmicro/home/base/CODE/MATLAB/recontree/compiled/compiledfiles_skelh5/cluster_skelh5'
+%compiledfunc = '/groups/mousebrainmicro/home/base/CODE/MATLAB/recontree/compiled/compiledfiles_skelh5/cluster_skelh5'
 %find number of random characters to choose from
 numRands = length(s);
 %specify length of random string to generate
@@ -376,7 +381,8 @@ for idx = 1:size(bbox,1)
     outfile = fullfile(outfolder,sprintf('%s_idx-%05d_stxyzendxyz-%d_%d_%d_%d_%d_%d.txt',bb,idx,bbox(idx,1:2:end),bbox(idx,2:2:end)));
     cluster_skelh5(myh5,myh5prob,BB,outfile,configfile)
 end
-end
+end  % runlocal()
+
 
 function deployment(configfile,mysh,myh5prob)
 %qsub -pe batch 4 -l short=true -N tile_test -j y -o ~/logs -b y -cwd -V './compiledfiles_mytest/mytest > output_mytest.log'
@@ -400,6 +406,7 @@ if nargin<3
     myh5prob = '/prob0'
 end
 if 1
+    % likely breakpoint location
     [brainSize,RR,chunk_dims,rank] = h5parser(myh5,myh5prob);
     % get a multiple of chunksize that is around 1000^3
     cropSize = round(1000./chunk_dims).*chunk_dims;
@@ -418,12 +425,15 @@ unix(sprintf('chmod g+rwx %s',outfolder));
 %
 % rmdir(outfolder)
 s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-old = 0;
-if old
-    compiledfunc = '/groups/mousebrainmicro/home/base/CODE/MATLAB/recontree/compiled/compiledfiles_skelh5/cluster_skelh5'
-else
-    compiledfunc = '/groups/mousebrainmicro/home/base/CODE/MATLAB/compiledfunctions/skeletonization/cluster_skelh5'
-end
+%old = 0;
+% if old
+%     compiledfunc = '/groups/mousebrainmicro/home/base/CODE/MATLAB/recontree/compiled/compiledfiles_skelh5/cluster_skelh5'
+% else
+%     compiledfunc = '/groups/mousebrainmicro/home/base/CODE/MATLAB/compiledfunctions/skeletonization/cluster_skelh5'
+% end
+script_file_path = mfilename('fullpath') ;
+script_folder_path = fileparts(script_file_path) ;
+compiledfunc = fullfile(script_folder_path, 'compiled', 'run_cluster_skelh5_at_janelia.sh') ;
 
 
 %find number of random characters to choose from
@@ -459,6 +469,7 @@ if 1 % check any missing file
 end
 sum(~finished)
 %%
+% likely breakpoint location
 iter=0;
 fid = fopen(mysh,'w');
 for idx = 1:size(bbox,1)
@@ -475,13 +486,17 @@ for idx = 1:size(bbox,1)
     name = sprintf('skel_%05d-%s',idx,randString);
     argsout = sprintf('''%s %s %s "[%d,%d,%d,%d,%d,%d]" %s %s''',compiledfunc,myh5,myh5prob,(BB),outfile,configfile);
     % mysub = sprintf('qsub -pe batch %d -l d_rt=%d -N %s -j y -o /dev/null -b y -cwd -V %s\n',numcores,timelim,name,args);
-    mysub = sprintf('bsub -n%d -R"affinity[core(1)]" -We %d -J %s -o %s %s\n',numcores,timelim/60,name,'/dev/null',argsout);
+    mysub = sprintf('bsub -P mouselight -n%d -R"affinity[core(1)]" -We %d -J %s -o %s %s\n',numcores,timelim/60,name,'/dev/null',argsout);
     fwrite(fid,mysub);
     iter=iter+1;
     sprintf('iter: %d',iter)
+%     % for debugging
+%     if iter==100 ,
+%         break
+%     end
 end
+fclose(fid) ;
 unix(sprintf('chmod +x %s',mysh));
-fclose(fid)
 % %%
 % % test
 %%
